4dd7b5112094ec5974b39d4a64c85e214951d22d
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-12-02 23:03:44 +0000

    profile page added

---
+++ app/profile/page.jsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Breadcrumb from "../components/breadcumb";
+
+export default function ProfilePage() {
+  const [activeTab, setActiveTab] = useState("daily");
+  const router = useRouter();
+
+  return (
+    <div className="bg-green-700  min-h-screen w-full py-6 px-5 md:px-8 lg:px-20 text-white">
+      <div className="mx-auto w-full max-w-2xl">
+        <Breadcrumb currentPage="Profil" />
+      </div>
+      {/* Profile + Info */}
+      <div className="flex items-center justify-evenly gap-4">
+        {/* Avatar */}
+        <div className=" rounded-full overflow-hidden bg-white shadow flex-shrink-0">
+          <Image
+            src="/avatar.png"
+            alt="Avatar"
+            width={140}
+            height={140}
+            className="object-cover"
+          />
+        </div>
+
+        {/* Info Box */}
+        <div className="bg-green-400/50  w-[40%] md:w-[25%] rounded-lg p-3 text-sm md:text-base">
+          <p className="font-semibold text-lg md:text-xl">Muhsin</p>
+          <p>
+            Mental Skor: <span className="font-bold">5/5</span>
+          </p>
+          <p>
+            Sağlık Skoru: <span className="font-bold">5/5</span>
+          </p>
+          <p>
+            Alışkanlık Skoru: <span className="font-bold">5/5</span>
+          </p>
+          <p>
+            Rozetleriniz: <span className="font-bold">12</span>
+          </p>
+        </div>
+      </div>
+
+      {/* Emoji Section */}
+      <div className="text-center my-14">
+        <p className="mb-2 text-sm md:text-base">
+          Bugün nasıl hissediyorsunuz?
+        </p>
+        <div className="flex justify-center gap-3 text-2xl md:text-3xl">
+          {["😭", "😔", "😐", "😲", "😀", "😁"].map((emoji, index) => (
+            <button
+              key={index}
+              className="hover:scale-125 transition-transform duration-150"
+            >
+              {emoji}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* Tabs */}
+      <div className="mt-16 flex justify-around text-sm font-semibold">
+        <button
+          onClick={() => setActiveTab("daily")}
+          className={`flex flex-col items-center gap-1 ${
+            activeTab === "daily" ? "text-yellow-300" : "text-white"
+          }`}
+        >
+          <span className="text-xl">🕘</span>
+          Günlük Görevler
+        </button>
+
+        <button
+          onClick={() => setActiveTab("goals")}
+          className={`flex flex-col items-center gap-1 ${
+            activeTab === "goals" ? "text-yellow-300" : "text-white"
+          }`}
+        >
+          <span className="text-xl">❤️</span>
+          Hedefler
+        </button>
+      </div>
+      <div
+        className="bg-yellow-400 w-screen relative left-1/2 right-1/2 
+    -ml-[50vw] -mr-[50vw] text-center py-14 md:py-20 mt-6"
+      >
+        <div className="max-w-4xl mx-auto grid grid-cols-1 gap-6 md:gap-8  p-6">
+          {activeTab === "daily" && (
+            <>
+              <div className="text-3xl md:text-4xl font-bold bg-white/30 py-4 rounded-xl shadow">
+                Kitap Oku
+              </div>
+
+              <div className="text-3xl md:text-4xl font-bold bg-white/30 py-4 rounded-xl shadow">
+                Dua Et
+              </div>
+            </>
+          )}
+
+          {activeTab === "goals" && (
+            <>
+              <div className="text-3xl md:text-4xl font-bold bg-white/30 py-4 rounded-xl shadow">
+                10 Sayfa Kitap
+              </div>
+
+              <div className="text-3xl md:text-4xl font-bold bg-white/30 py-4 rounded-xl shadow">
+                5 Vakit Namaz
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+      <div className="mt-16">
+        <p className="py-2 text-center bg-white text-gray-700 text-xs w-[40%] md:w-[25%] mx-auto mt-4 rounded-md shadow-sm">
+          Welcome!
+        </p>
+      </div>
+    </div>
+  );
+}
